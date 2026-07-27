cc9f83441906133b034b38b47663c3949b9596b4
Jonas Salo <salomannen1234@gmail.com>
2026-07-27 21:20:07 +0900

    macanc: ship Oplus feature flags

--- macanc-vendor.mk
+++ macanc-vendor.mk
@@ -1050,6 +1050,7 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/macanc/proprietary/odm/lib64/camera/libpdCustomParam_Cam3.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/libpdCustomParam_Cam3.bin \
     vendor/oneplus/macanc/proprietary/odm/lib64/camera/revision.txt:$(TARGET_COPY_OUT_ODM)/lib64/camera/revision.txt \
     vendor/oneplus/macanc/proprietary/odm/vendor/etc/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_ODM)/vendor/etc/wifi/WCNSS_qcom_cfg.ini \
+    vendor/oneplus/macanc/proprietary/product/etc/permissions/oplus.feature.android.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/oplus.feature.android.xml \
     vendor/oneplus/macanc/proprietary/vendor/etc/display_apollo_list_AA607_P_7_A0020_dsc_cmd_mode_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display_apollo_list_AA607_P_7_A0020_dsc_cmd_mode_panel.xml \
     vendor/oneplus/macanc/proprietary/vendor/etc/init/vendor.qti.camera.provider-service_64.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.camera.provider-service_64.rc \
     vendor/oneplus/macanc/proprietary/vendor/etc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
